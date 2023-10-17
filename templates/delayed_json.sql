5a0e537f2e0baff56d57ca503b314122e70a8775
vykuang <vykuang92@gmail.com>
2023-10-17 21:37:00 +0800

    use execute instead of executescript to return query result

--- templates/delayed_json.sql
+++ templates/delayed_json.sql
@@ -1,11 +1,11 @@
 WITH RECURSIVE 
 delays(flight_date, start, dest, delay) as (
     SELECT
-        json_extract({colname_json},'$.flight_date') as date,
-        json_extract({colname_json},'$.departure.airport') as start,
-        json_extract({colname_json},'$.arrival.airport') as dest,
-        CAST(json_extract({colname_json},'$.arrival.delay') AS INTEGER) as delay
-    FROM {tblname_json}
+        json_extract({{json_col}},'$.flight_date') as flight_date,
+        json_extract({{json_col}},'$.departure.airport') as start,
+        json_extract({{json_col}},'$.arrival.airport') as dest,
+        CAST(json_extract({{json_col}},'$.arrival.delay') AS INTEGER) as delay
+    FROM {{tbl_name}}
 )
 SELECT
     flight_date,
